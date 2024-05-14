a9fb075cea0b94cfeb9b32c36148a02b87f7f9f4
Maksim Baranov <mclerishere@gmail.com>
2024-05-14 21:00:33 +0200

    test: added empty output path test

---
+++ test/cases/empty-output-path/webpack.config.js
@@ -0,0 +1,15 @@
+import ConcatPlugin from '../../../index';
+
+module.exports = {
+    entry: './index.js',
+    plugins: [
+        new ConcatPlugin({
+            name: 'file',
+            fileName: '[name].js',
+            filesToConcat: ['../../fixtures/a.js', '../../fixtures/b.js', 'is-object'],
+        })
+    ],
+    optimization: {
+        minimize: false,
+    },
+};
